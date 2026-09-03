4fe19411994a2c41c07690a670333aab6c2c821c
Ivan Trunaev <trunaev.ivan.1987@gmail.com>
2026-09-03 14:02:32 +0500

    Samples generator fix

--- generate-samples.pl
+++ generate-samples.pl
@@ -8,5 +8,5 @@ use Hacker;
 
 
 Hacker->render('samples/sine.pcm' => map {sin($_ * 440 * 3.14159265359 / 44100.0)} 0 .. 44100 * 5);
-Hacker->render(map {Hacker->sun($_ * 330 / 44100.0)} 0 .. 44100 * 5);
+Hacker->render('samples/sun.pcm' => map {Hacker->sun($_ * 330 / 44100.0)} 0 .. 44100 * 5);
 1;
