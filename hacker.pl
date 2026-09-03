53e275ea43a8df306e20ca08f98461b9d9d2a4bf
Ivan Trunaev <trunaev.ivan.1987@gmail.com>
2026-09-03 17:05:33 +0500

    Project manager

---
+++ hacker.pl
@@ -0,0 +1,38 @@
+#!/usr/bin/perl
+
+use 5.022;
+use warnings;
+
+use lib 'lib';
+
+use Getopt::Long qw(GetOptions);
+
+use Hacker;
+
+my ($driver, $help, $play, $render);
+
+GetOptions('driver=s' => \$driver, 'play' => \$play, 'render=s' => \$render, 'h|help' => \$help);
+usage() if $help;
+sub usage { say "Usage:\n\t$0 [--driver=...] [--help] [--play] [--render=...] project-file.hacker" and exit(); }
+
+my $project = pop @ARGV or die "Project file is not specified";
+die "No project file $project" unless -f $project;
+
+my $proj;
+my $fi;
+open $fi, $project;
+$proj = join '', <$fi>;
+close $fi;
+
+my @signal = eval $proj;
+
+die $@ if $@;
+
+unless ($render) {
+	Hacker->play(@signal);
+} else {
+	Hacker->render($render => @signal);
+}
+
+
+1;
