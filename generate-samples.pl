45e2f607f1a998baac795905fdf92fcfed8b1d08
Ivan Trunaev <trunaev.ivan.1987@gmail.com>
2026-09-03 14:36:23 +0500

    Sun 8 bit sample

--- generate-samples.pl
+++ generate-samples.pl
@@ -10,4 +10,6 @@ use Hacker;
 Hacker->render('samples/sine.pcm' => map {sin($_ * 440 * 3.14159265359 / 44100.0)} 0 .. 44100 * 5);
 Hacker->render('samples/sun.pcm' => map {Hacker->sun($_ * 330 / 44100.0)} 0 .. 44100 * 5);
 Hacker->render('samples/saw.pcm' => map {int (Hacker->sun($_ * 110 / 44100.0)+0.5)} 0 .. 44100 * 5);
+
+Hacker->render('samples/sun-8bit.pcm' => map {int(Hacker->sun($_ * 220 / 44100.0)*3)/3} 0 .. 44100 * 5);
 1;
