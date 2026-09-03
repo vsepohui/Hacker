1b81e7273f72ccd5be131d01d8b0f34a3510a53b
Ivan Trunaev <trunaev.ivan.1987@gmail.com>
2026-09-03 16:45:37 +0500

    FIRST PERL SONG DEMO GENERATOR: UPDATE, FIRST CLEAN MASTER MIXER DENO

--- song-one-hand-clapping.pl
+++ song-one-hand-clapping.pl
@@ -10,11 +10,25 @@ use Hacker::Synth::Triangle;
 
 my @seq = qw[0 4 5 0 4 5 0 4 5 7 7 7 16 12 9 16 12 9 16 12 9 7 7 7] x2;
 
-my @signal;
+my @channel_1;
+my @channel_2;
 for (@seq) {
-	push @signal, Hacker::Synth::Triangle->signal($_ - 1, 0.5);
+	push @channel_1, Hacker::Synth::Triangle->signal($_ - 1, 0.5);
+	push @channel_2, Hacker::Synth::Triangle->signal($_ - 1 + 12, 0.5);
 }
 
-Hacker->play(@signal);
+
+my @master;
+
+my $max = 0;
+for (my $i = 0 ; $i < @channel_1 ; $i ++) {
+	my $s = $channel_1[$i] + $channel_2[$i];
+	$max = abs($s) if abs($s) > $max;
+	push @master, $s;
+}
+
+@master = map {$_ / $max} @master;
+
+Hacker->play(@master);
 
 1;
