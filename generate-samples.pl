41e6cfa72c1e376c5848ab178edd22fe98ecc79a
Ivan Trunaev <trunaev.ivan.1987@gmail.com>
2026-09-03 19:02:44 +0500

    Update, new project skel

--- generate-samples.pl
+++ generate-samples.pl
@@ -10,12 +10,12 @@ use Hacker::Synth::Saw;
 use Hacker::Synth::Triangle;
 
 
-Hacker->render('samples/sine.pcm' => map {sin($_ * 440 * 3.14159265359 / 44100.0)} 0 .. 44100 * 5);
-Hacker->render('samples/sun.pcm' => Hacker::Synth::Sun->signal);
-Hacker->render('samples/saw.pcm' => Hacker::Synth::Saw->signal);
+Hacker->render('Samples/sine.pcm' => map {sin($_ * 440 * 3.14159265359 / 44100.0)} 0 .. 44100 * 5);
+Hacker->render('Samples/sun.pcm' => Hacker::Synth::Sun->signal);
+Hacker->render('Samples/saw.pcm' => Hacker::Synth::Saw->signal);
 
-Hacker->render('samples/sun-8bit.pcm' => map {int(Hacker->sun($_ * 220 / 44100.0)*3)/3} 0 .. 44100 * 5);
-Hacker->render('samples/triangle.pcm' => Hacker::Synth::Triangle->signal);
+Hacker->render('Samples/sun-8bit.pcm' => map {int(Hacker->sun($_ * 220 / 44100.0)*3)/3} 0 .. 44100 * 5);
+Hacker->render('Samples/triangle.pcm' => Hacker::Synth::Triangle->signal);
 
 
 1;
