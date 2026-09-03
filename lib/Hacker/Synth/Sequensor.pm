e3c14ca0e750e0acf56daf363e971c8b0a400175
Ivan Trunaev <trunaev.ivan.1987@gmail.com>
2026-09-03 20:42:50 +0500

    Perfect Sequencor without overflows

---
+++ lib/Hacker/Synth/Sequensor.pm
@@ -0,0 +1,82 @@
+package Hacker::Synth::Sequensor;
+
+use 5.022;
+use warnings;
+
+use base 'Hacker::Synth';
+
+use Hacker::Effect::Transpose;
+
+sub new {
+	my $class = shift;
+	my %opts = (
+		sample => undef,
+		bpm    => 120,
+		@_,
+	);
+
+	my $sample 	= $opts{sample} or die "Sample is not specified";
+	my $bpm 	= $opts{bpm} or die "BPM is not specified";
+		
+	my $self = {
+		sample	=> $sample,
+		bpm 	=> $bpm,
+	};
+	
+	return bless $self, $class;
+}
+
+
+sub signal {
+	my $self    = ref $_[0] ? shift : new shift;
+	my $pattern = shift;
+	#$length //= scalar @{$self->{sample}} - 1;
+	
+	return $self->render_pattern($pattern);
+}
+
+
+
+sub render_pattern {
+	my $self    = shift;
+	my $pattern = shift;
+	
+	my @steps = split /,/, $pattern;
+	
+	my $step_time = $self->{bpm} / (60 * 4);
+	my $max = $step_time * scalar (@steps) * 44100;
+	
+	my $next_step = 1;
+	my $current_step = -1;
+	my $offset = 0;
+	my @buff;
+	my @s;
+	
+	for (my $i = 0 ; $i < $max ; $i ++) {
+		#warn scalar @s;
+		my $step = int $i / (44100 * $step_time);
+		if ($step == $current_step) { 
+			push @s, @buff && $buff[$offset] ? $buff[$offset] : 0;
+		} else {
+			$current_step = $step;
+			my $x = $steps[$step];
+			if ($x =~ /^-?\d+$/) {
+				$offset = 0;
+				@buff = Hacker::Effect::Transpose->new(value => $x)->process(@{$self->{sample}});
+				push @s, $buff[$offset];
+			} elsif ($x eq '#') {
+				@buff = ();
+				push @s, 0;
+			} else {
+				push @s, @buff && $buff[$offset] ? $buff[$offset] : 0;
+			}
+			
+		}
+		
+		$offset ++;
+	}
+
+	return @s;
+}
+
+1;
